2c56da1623100d373aca30cc396910118d7ef93c
vanGalilea <z.galili@icloud.com>
2017-07-19 11:55:56 +0200

    prop-types, MUI and othe dependencies installed and configured

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import PropTypes from 'prop-types'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import muiTheme from './assets/styles/theme'
 import './App.css';
 
 class App extends Component {
+  static childContextTypes = {
+    muiTheme: PropTypes.object.isRequired,
+  }
+
+  getChildContext() {
+    return { muiTheme }
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <div className="App">
           <h2>Welcome to React</h2>
+          <p>blabladbladlbalb</p>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      </MuiThemeProvider>
+    )
   }
 }
 
